8b927d8870e421608cd429b19b670a39d77d64b6
PolarDi <dimon1gameswww@gmail.com>
2025-05-27 00:38:57 +0300

    Добавлена проверка роли teacher в UserController

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class UserController extends Controller
 {
